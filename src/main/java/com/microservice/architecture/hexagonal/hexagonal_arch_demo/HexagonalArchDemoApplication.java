c42409703e508ddd74a16b19b7c64e73882ce5f4
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-28 12:14:12 +0000

    initial commit

---
+++ src/main/java/com/microservice/architecture/hexagonal/hexagonal_arch_demo/HexagonalArchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.architecture.hexagonal.hexagonal_arch_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/architecture/hexagonal/hexagonal_arch_demo/HexagonalArchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.architecture.hexagonal.hexagonal_arch_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
